0f455495b794decc2bc61fae1c4f7b3a09bf5295
mahabubr <mahabub.r753@gmail.com>
2023-10-14 15:52:10 +0600

    service pagination and searbar implement

--- src/components/Services/ServicePriceHighToLow.tsx
+++ src/components/Services/ServicePriceHighToLow.tsx
@@ -2,12 +2,32 @@ import { useGetServicesQuery } from "@/redux/api/features/services/servicesApi";
 import Loader from "../Shared/Loader";
 import ServiceCard from "./Card/ServiceCard";
 import { iServices } from "@/interface/api";
+import XPagination from "../Shared/Pagination";
+import { useState } from "react";
+import { Input } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+import { useDebounced } from "@/redux/hooks/useDebounced";
 
 const ServicePriceHighToLow = () => {
-  const query: any = {};
+  const query: Record<string, any> = {};
 
+  const [page, setPage] = useState<number>(1);
+  const [size, setSize] = useState<number>(10);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  query["limit"] = size;
+  query["page"] = page;
   query["sortBy"] = "price";
   query["sortOrder"] = "asc";
+
+  const debouncedSearchTerm = useDebounced({
+    searchQuery: searchTerm,
+    delay: 600,
+  });
+
+  if (!!debouncedSearchTerm) {
+    query["searchTerm"] = debouncedSearchTerm;
+  }
 
   const { data, isLoading } = useGetServicesQuery(query);
 
@@ -15,17 +35,41 @@ const ServicePriceHighToLow = () => {
     return <Loader />;
   }
 
+  const meta = data?.data?.meta;
+
+  const handlePaginationChange = (page: number, pageSize: number) => {
+    setPage(page);
+    setSize(pageSize);
+  };
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-      {data?.data?.data?.length > 0 &&
-        data.data.data.map((service: iServices) => (
-          <ServiceCard
-            key={service.id}
-            service={service}
-            isLoading={isLoading}
-          />
-        ))}
-    </div>
+    <>
+      <Input
+        size="large"
+        placeholder="Find your destination"
+        className="my-5"
+        onChange={(e) => setSearchTerm(e.target.value)}
+        suffix={<SearchOutlined />}
+      />
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {data?.data?.data?.length > 0 &&
+          data.data.data.map((service: iServices) => (
+            <ServiceCard
+              key={service.id}
+              service={service}
+              isLoading={isLoading}
+            />
+          ))}
+      </div>
+      <div className="my-10 flex justify-end">
+        <XPagination
+          handlePaginationChange={handlePaginationChange}
+          meta={meta}
+          size={size}
+        />
+      </div>
+    </>
   );
 };
 
